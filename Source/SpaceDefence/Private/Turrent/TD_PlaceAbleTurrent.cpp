f3660d7eab7854ec9943dd3a895134925a81120a
Balaji <balajiganeshoff@gmail.com>
2021-01-28 22:09:53 -0700

    Some Gun Models

---
+++ Source/SpaceDefence/Private/Turrent/TD_PlaceAbleTurrent.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Turrent/TD_PlaceAbleTurrent.h"
+
+// Sets default values
+ATD_PlaceAbleTurrent::ATD_PlaceAbleTurrent()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATD_PlaceAbleTurrent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATD_PlaceAbleTurrent::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
